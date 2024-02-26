1d4733df6e9460aacb01b9df74faaa3095367602
icalll <zalsujana07@gmail.com>
2024-02-26 15:01:21 +0700

    up

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './scss/style.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ""}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
